4e1b2c96849d8ffe0367206bb6618ddf8ae04662
Mango <thatonemango813@gmail.com>
2020-03-01 22:41:05 -0600

    Restructure project, attempt to get static files working with Django

--- cs4330/cs4330/settings.py
+++ cs4330/cs4330/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'project.apps.ProjectConfig',
 ]
 
 MIDDLEWARE = [
@@ -54,7 +55,7 @@ ROOT_URLCONF = 'cs4330.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ['./cs4330/views'],
+        'DIRS': ['./project/views'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -118,8 +119,3 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR,"./cs4330/CSS")
-    ]
--- cs4330/project/views.py
+++ cs4330/project/views.py
@@ -2,3 +2,6 @@ from django.shortcuts import render, redirect
 
 def login(request):
     return render(request, 'login.html')
+
+def register(request):
+    return render(request, 'register.html')
